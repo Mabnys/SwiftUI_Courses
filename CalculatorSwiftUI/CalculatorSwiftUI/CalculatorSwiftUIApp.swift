43582f536a4064d2bdbe02236e60d8d9ec02ee0d
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2023-12-29 09:18:02 -0500

    Project creation

---
+++ CalculatorSwiftUI/CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Mamadou Balde on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalculatorSwiftUI
+//
+//  Created by Mamadou Balde on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
