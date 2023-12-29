81425eb0e4fee9ecae2d59f31f7bebe6186bfae3
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2023-12-29 10:38:35 -0500

    Mastered stacking
    - created 2 dimensional grid of buttons
    - learned VStack, HStack and ZStack
    - looped through 2 dimensional array

--- CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    let buttons = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack (alignment: .bottom) {
+            Color.black.ignoresSafeArea(.all)
+            VStack (spacing: 12){
+                HStack {
+                    Spacer()
+                    Text("44")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 64))
+                }
+                .padding()
+                ForEach(buttons, id: \.self) { row in
+                    HStack (spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                 .font(.system(size: 32))
+                                 .frame(width: buttomWidth(), height: buttomWidth())
+                                 .foregroundColor(.white)
+                                 .background(Color.yellow)
+                                 .cornerRadius(buttomWidth())
+                        }
+                    }
+                }
+            }
+            .padding(.bottom)
         }
-        .padding()
+    }
+    
+    func buttomWidth() -> CGFloat {
+        (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
