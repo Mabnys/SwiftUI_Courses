b15f32d1afc2982c48d6630a647950c1c5c0a872
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-03 10:47:54 -0400

    Created a new project Bullseye

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Mamadou Balde on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
