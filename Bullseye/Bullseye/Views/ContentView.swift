422dfc50da275ceeeff2b69431b1e7db83c22bde
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-04 13:03:34 -0400

    Added Colors and Gradients

--- Bullseye/Bullseye/Views/ContentView.swift
+++ Bullseye/Bullseye/Views/ContentView.swift
@@ -13,41 +13,58 @@ struct ContentView: View {
   @State private var game = Game()
   
   var body: some View {
-    VStack {
-      Text("🎯🎯🎯\n PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-        .bold()
-        .multilineTextAlignment(.center)
-        .lineSpacing(4.0)
-        .font(.footnote)
-        .kerning(2.0)
-      Text(String(game.target))
-        .kerning(-1)
-        .font(.largeTitle)
-        .fontWeight(.black)
-      HStack {
-        Text("1")
+    ZStack {
+      Color("BackgroundColor")
+        .ignoresSafeArea()
+      VStack {
+        Text("🎯🎯🎯\n Put the bullseye as close as you can to".uppercased())
           .bold()
-        Slider(value: $sliderValue, in: 1.0...100.0)
-        Text("100")
-          .bold()
-      }
-      Button("Hit me") {
-        alertIsVisible = true
-      }
-      .alert("Hello there",
-             isPresented: $alertIsVisible,
-             actions: {
-        Button("Awesome") {
-          print("Alert closed")
+          .multilineTextAlignment(.center)
+          .lineSpacing(4.0)
+          .font(.footnote)
+          .kerning(2.0)
+          .padding(.horizontal, 30)
+        Text(String(game.target))
+          .kerning(-1)
+          .font(.largeTitle)
+          .fontWeight(.black)
+        HStack {
+          Text("1")
+            .bold()
+          Slider(value: $sliderValue, in: 1.0...100.0)
+          Text("100")
+            .bold()
         }
-      },
-             message: {
-        let roundedValue = Int(sliderValue)
-        Text("""
-          The slider's value is \(roundedValue).
-          You scored \(game.points(sliderValue: roundedValue)) points this round.
-          """)
-      })
+        .padding()
+        Button("Hit me".uppercased()) {
+          alertIsVisible = true
+        }
+        .padding(20.0)
+        .background(
+          ZStack {
+            Color("ButtonColor")
+            LinearGradient(colors: [Color.white.opacity(0.3), Color.clear], startPoint: .top, endPoint: .bottom)
+          }
+        )
+        .foregroundColor(.white)
+        .cornerRadius(20.0)
+        .bold()
+        .font(.title3)
+        .alert("Hello there",
+               isPresented: $alertIsVisible,
+               actions: {
+          Button("Awesome") {
+            print("Alert closed")
+          }
+        },
+               message: {
+          let roundedValue = Int(sliderValue)
+          Text("""
+            The slider's value is \(roundedValue).
+            You scored \(game.points(sliderValue: roundedValue)) points this round.
+            """)
+        })
+      }
     }
   }
 }
