8588be3c555c7b58a006d0a74af33469e1e3fb9d
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-04 00:06:25 -0400

    Applied type inference to optimize the code base

--- Bullseye/Bullseye/Views/ContentView.swift
+++ Bullseye/Bullseye/Views/ContentView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var alertIsVisible: Bool = false
-  @State private var sliderValue: Double = 50.0
-  @State private var game: Game = Game()
+  @State private var alertIsVisible = false
+  @State private var sliderValue = 50.0
+  @State private var game = Game()
   
   var body: some View {
     VStack {
@@ -42,7 +42,7 @@ struct ContentView: View {
         }
       },
              message: {
-        let roundedValue: Int = Int(sliderValue)
+        let roundedValue = Int(sliderValue)
         Text("""
           The slider's value is \(roundedValue).
           You scored \(game.points(sliderValue: roundedValue)) points this round.
