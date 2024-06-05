1321f59b7b907022fd246d2e29bd5e00217a0077
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-05 12:24:03 -0400

    Created a custom alert to be displayed for the user

--- Bullseye/Bullseye/Views/TextViews.swift
+++ Bullseye/Bullseye/Views/TextViews.swift
@@ -55,12 +55,43 @@ struct LabelText: View {
   }
 }
 
+struct BodyText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .font(.subheadline)
+      .fontWeight(.semibold)
+      .multilineTextAlignment(.center)
+      .lineSpacing(10)
+  }
+}
+
+struct ButtonText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .bold()
+      .padding()
+      .frame(maxWidth: .infinity)
+      .cornerRadius(12)
+      .background(
+        Color.accentColor
+      )
+      .foregroundColor(.white)
+  }
+}
+
 #Preview {
   VStack {
     InstructionText(text: "Instructions")
     BigNumberText(text: "999")
     SliderLabelText(text: "99")
     LabelText(text: "Score")
+    BodyText(text: "You scored 200 Points\n 😻😻😻")
+    ButtonText(text: "Start New Round")
   }
+  .padding()
 }
 
