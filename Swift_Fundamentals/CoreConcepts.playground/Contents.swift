a93c663639f76df94c9e067339d0ac16232c6c20
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-12 12:31:17 -0400

    Practiced Booleans & Comparison Operators

--- Swift_Fundamentals/CoreConcepts.playground/Contents.swift
+++ Swift_Fundamentals/CoreConcepts.playground/Contents.swift
@@ -13,3 +13,53 @@ let goodByeMessage = "See you soon!"
  also, a haiku!*/
 
  print(goodByeMessage)
+
+// Booleans & Comparison Operators
+
+//let yes: Bool = true
+let yes = true
+
+//let no: Bool = false
+let no = false
+
+// You can also declare a boolean using "Expression" (it's a unit of code that resolves to a value)
+
+/* for example:
+ if difference == 0 {
+ ...
+ }
+ The part that says "difference == 0" is the expression, .
+ In this case the expression resolve to:
+ true or false.
+ Now the result of this expression is a temporary boolean value. */
+
+let passingGrade = 50
+
+//let studentGrade = 74
+let studentGrade = 50
+
+//let studentPassed = studentGrade > passingGrade
+let studentPassed = studentGrade >= passingGrade
+
+let studentFailed = studentGrade < passingGrade
+
+let chrisGrade = 49
+let samGrade = 99
+
+// = is used to assigned values
+// == is the equality operator
+
+samGrade == chrisGrade
+samGrade != chrisGrade
+
+let catName = "Ozma"
+let dogName = "Mango"
+catName == dogName
+catName > dogName /* It is "true" because catName comes first then dogName.
+                   Ask this question: Does this string comnes before or after another string 
+                   Ozma comes after Mango alphabetically speaking,
+                   so this statement is true */
+
+// Comparison operators are used troughtout a programer career, thus, it is great idea to understand them really well.
+
+
