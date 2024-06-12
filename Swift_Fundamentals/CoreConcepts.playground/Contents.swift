2caa492fd64bf60c786caa06b70a3f112fe0a2c2
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-12 12:03:46 -0400

    learned Swift Playgrounds & Comments

--- Swift_Fundamentals/CoreConcepts.playground/Contents.swift
+++ Swift_Fundamentals/CoreConcepts.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
+// Introduction
+
 var greeting = "Hello, playground"
+
+let goodByeMessage = "See you soon!"
+
+// The code below prints out a goodbye message
+
+/* This is a comment
+ spread out over a few lines
+ also, a haiku!*/
+
+ print(goodByeMessage)
