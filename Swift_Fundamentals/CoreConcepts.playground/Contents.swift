8599d41848052af21b03a264a642e425c908390d
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-12 12:54:34 -0400

    completed the challenges for Booleans & Comparison Operators

--- Swift_Fundamentals/CoreConcepts.playground/Contents.swift
+++ Swift_Fundamentals/CoreConcepts.playground/Contents.swift
@@ -58,8 +58,39 @@ catName == dogName
 catName > dogName /* It is "true" because catName comes first then dogName.
                    Ask this question: Does this string comnes before or after another string 
                    Ozma comes after Mango alphabetically speaking,
-                   so this statement is true */
+                   so this statement is true 
+                   Note that with strings, "<" really means comes ealier in the dictionary. */
 
 // Comparison operators are used troughtout a programer career, thus, it is great idea to understand them really well.
+
+// Challenge - Booleans & Comparison Operators
+
+/* Challenge 1- Create a constant named myAge and set its value to your age.
+ Then, create a constant named isVotingAge that uses Boolean logic to determine if the value stored in myAge denotes someone of voting age.
+ In my part of the world, the voting age is 18, so I'll use that here. */
+
+// Solution 1:
+let myAge = 29
+let isVotingAge = myAge >= 18
+
+/* Challenge 2- Create a constant named student and set its value to your name as a string.
+ Next, create a constant named author and set its value to "Matt Galloway", the original author of these exercices.
+ Then, create a third constant named authorIsStudent that uses string equality to determine if the values of student and author are equal. */
+
+// Solution 2:
+let student = "Mamadou Balde"
+let author = "Matt Galloway"
+let authorIsStudent = student == author
+
+
+/* Challenge 3- Create a constant named studentBeforeAuthor which uses string comparison to determine if the string value in the constant student comes, alphabeltically speaking, before the string value in the constant author.
+ The constants student and an author were declared above in Challenge 2, so you don't need to redeclare them here. */
+
+// Solution 3:
+let studentBeforeAuthor = student < author // This is the solution
+// let studentBeforeAuthor = student > author // We could also use ">"
+
+
+
 
 
