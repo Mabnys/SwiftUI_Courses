353f34b3600c8a23e12ebf3d9f9164fa7120a06d
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-13 23:34:01 -0400

    completed the challenges related to Iterating Collections

--- Swift_Fundamentals/CoreConcepts.playground/Contents.swift
+++ Swift_Fundamentals/CoreConcepts.playground/Contents.swift
@@ -606,4 +606,37 @@ for temperature in poolTemperatures {
 }
 sums / poolTemperatures.count
 
+// - Challenge: Iterating Collections
 
+// Challenge 1-
+/*
+I've provided an array of pastries below.
+ Create a loop that iterates over each element of the array, and uses an if statement inside the loop to print out the pastries that start with the letter "c".
+ There's no need to use i or another index variable to loop through the array; use the more compact way to
+ iterate over every element of an array.
+ To check if first letter of a string matches a certain character, you can use code of the following form:
+ if pastry[pastry.startIndex] == "c"
+ */
+
+print("Solution 1")
+// Solution 1:
+var pastriess: [String] = ["cookie", "danish", "cupcake", "donut", "pie", "brownie", "fritter", "cruller"]
+
+for pastry in pastriess {
+  if pastry[pastry.startIndex] == "c" {
+    print(pastry)
+  }
+}
+
+// Challenge 2-
+/*
+ You can write an even more compact form of the loop above, using a where clause as part of your for loop condition.
+ Remove the if block inside your loop; the only statement inside your loop should be print(pastry).
+ Add a where clause to your for loop with the same if condition you used in the loop above.
+ Your output should be the same as in Challenge 1.
+ */
+
+// Solution 2: // Note that writing tidy and compact code is the mark of a good developer. So let's try and tighten up the code from challenge 1.
+for pastry in pastriess where pastry[pastry.startIndex] == "c" {
+  print(pastry)
+}
