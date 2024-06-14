82b0f6fc24caef267f544f7198e674297293575d
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-14 01:53:10 -0400

    learned about Creating & Populating Sets

--- Swift_Fundamentals/CoreConcepts.playground/Contents.swift
+++ Swift_Fundamentals/CoreConcepts.playground/Contents.swift
@@ -914,3 +914,30 @@ for (key, value) in mamadou {
                               */
 }
 print("-----")
+
+// Working with Sets
+/*
+ A set is an unordered collection of unique values, of the same type.
+ */
+
+// Creating and Populating Sets
+var someSet: Set<Int> = [1, 2, 3, 1]
+// let someArray: Array<Int>
+// let someDictionary: Dictionary<String, Int>
+someSet.contains(1)
+someSet.contains(99)
+someSet.insert(5)
+someSet.remove(3)
+let removedElement = someSet.remove(3)
+let nilElement = someSet.remove(42)
+print(someSet)
+
+let anotherSet: Set<Int> = [5, 7, 13]
+let intersection = someSet.intersection(anotherSet)
+let difference = someSet.symmetricDifference(anotherSet)
+let union = someSet.union(anotherSet)
+someSet.formUnion(anotherSet)
+print(intersection)
+print(difference)
+print(union)
+print(someSet)
