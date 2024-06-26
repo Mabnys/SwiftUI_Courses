6c1c22650fc0f4f5f15dd9118bb2d59668b8c98e
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-06-26 12:05:50 -0400

    Refactored the code and showed more detail to the DetailView

--- SwiftUI-Navigation/Starter/PublicArt/DetailView.swift
+++ SwiftUI-Navigation/Starter/PublicArt/DetailView.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2023 Kodeco Inc.
-///
+/// Copyright (c) 2024 Kodeco Inc.
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,28 +32,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  let artworks = artData
-
+struct DetailView: View {
+  let artwork: Artwork
+  
   var body: some View {
-    NavigationStack {
-      List(artworks) { artwork in
-        NavigationLink(value: artwork) {
-          Text(artwork.title)
-        }
-      }
-      .navigationDestination(for: Artwork.self, destination: { artwork in
-        DetailView(artwork: artwork)
-      })
-      .navigationBarTitle("Artworks")
+    VStack {
+      Image(artwork.imageName)
+        .resizable()
+        .frame(maxWidth: 300, maxHeight: 600)
+        .aspectRatio(contentMode: .fit)
+      Text("\(artwork.reaction) \(artwork.title)")
+        .font(.headline)
+        .multilineTextAlignment(.center)
+        .lineLimit(3)
+      Text(artwork.locationName)
+        .font(.subheadline)
+      Text("Artist: \(artwork.artist)")
+        .font(.subheadline)
+      Divider()
+      Text(artwork.description)
+        .multilineTextAlignment(.leading)
+        .lineLimit(20)
     }
+    .padding()
+    .navigationBarTitle(Text(artwork.title), displayMode: .inline)
   }
 }
 
 
-
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+#Preview {
+  DetailView(artwork: artData[0])
 }
