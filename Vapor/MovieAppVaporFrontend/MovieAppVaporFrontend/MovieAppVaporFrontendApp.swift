5efec4b0702f66ce6506211e1e24c7549fadf4c0
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-11-07 11:23:35 -0500

    Created a new MovieAppVaporFrentEnd

---
+++ Vapor/MovieAppVaporFrontend/MovieAppVaporFrontend/MovieAppVaporFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppVaporFrontendApp.swift
+//  MovieAppVaporFrontend
+//
+//  Created by Mamadou Balde on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppVaporFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vapor/MovieAppVaporFrontend/MovieAppVaporFrontendTests/MovieAppVaporFrontendTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppVaporFrontendTests.swift
+//  MovieAppVaporFrontendTests
+//
+//  Created by Mamadou Balde on 11/7/24.
+//
+
+import Testing
+@testable import MovieAppVaporFrontend
+
+struct MovieAppVaporFrontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
